01a5d41863177fbf0a100c2d2232d67609a993b7
hex <hex1@foxmail.com>
2024-05-22 00:13:34 +0800

    项目初始化

---
+++ src/main/java/com/hex/n0001/TwoSum.java
@@ -0,0 +1,17 @@
+package com.hex.n0001;
+
+/**
+ * Copyright (C), 1999-2999, HEX Co., Ltd.
+ *
+ * @Description TwoSum
+ * @Author 14356
+ * @Date 2024.05.21 23:57
+ * @Version v1.0
+ */
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        return nums;
+    }
+}
